aa87728e78557a7f8c39d4fb94a648a62900b75f
ryan-heslin <rh3788@american.edu>
2022-02-06 13:56:18 -0500

    Small updates

--- solutions/day24.py
+++ solutions/day24.py
@@ -6,98 +6,129 @@ with open("inputs/day24.txt") as f:
 
 instr_length = len(raw_input) // 14
 
-processed = [
-    [y.rstrip("\n") for y in raw_input[(x + 1) : (x + instr_length)]]
-    for x in range(0, len(raw_input), instr_length)
-]
+constant_lines = [4, 5, 15]
+constant_args = ["c4", "c5", "c15"]
+processed = [x.rstrip("\n") for x in raw_input]
+values = []
+for x in range(0, len(raw_input), instr_length):
+    values.append(
+        dict(
+            zip(
+                constant_args,
+                [
+                    int(re.match(r"^[^0-9\-]+(-?\d+)$", line).group(1))
+                    for line in (processed[x] for x in constant_lines)
+                ],
+            )
+        )
+    )
+    constant_lines = [x + instr_length for x in constant_lines]
+# print(f"Answer 2 = {answer2}")
+values = {i: d for i, d in enumerate(values)}
+
+# z is positive definite
+@lru_cache(maxsize=512)
+def block(w, z, c4, c5, c15):
+    x = int((z % 26 + c5) != w)
+    z = (z // c4) * (25 * x + 1) + (w + c15) * x
+    return z
 
 
-def extract_code(
-    instr, ops={"add": "+", "mul": "*", "div": "//", "mod": "%", "eql": "=="}
-):
-    pattern = re.compile("^([a-z]{3}) ([wxyz]) (-?[0-9wxyz]+)$")
-    matches = list(re.match(pattern, instr).groups())
-    op = ops[matches[0]]
-    eq = ("int(", ")") if op == "==" else ("", "")
-    return f"{matches[1]} = {eq[0]}{matches[1]} {op} {matches[2]}{eq[1]}"
-
-
-def constructor(code):
-    cache = {}
-
-    def inner(state):
-        key = tuple(state.values())
-        if key not in cache.keys():
-            exec(code)
-            cache[key] = state
-        return cache[key]
-
-    return inner
-
-
-def constructor2(code):
-    @lru_cache(maxsize=None)
-    def inner(state):
-        exec(code)
-        return state
-
-    return inner
-
-
-def search(num, funs, powers):
-    assert num < 10 ** 14
-
-    state = {"x": 0, "y": 0, "z": 0}
-    for i in range(len(powers)):
-        digit = num // powers[i]
-        num = num % powers[i]
-        # Update w with new digit and run next block with previous state
-        state["w"] = digit
-        state = funs[i](state)
-    return state["z"]
+def search(num, powers, maxes, constant_args):
+    z = 0
+    while True:
+        for i in range(len(powers)):
+            digit = num // powers[i]
+            num = num % powers[i]
+            # Update w with new digit and run next block with previous state
+            z = block(w=digit, z=z, **constant_args[i])
+            if z > maxes[i]:
+                break
+        yield z
 
 
 def nonzero_subtract(num):
-    num -= 1
-    while "0" in str(num):
+    while num:
         num -= 1
-    return num
+        if not "0" in str(num):
+            yield num
 
 
-def solve(funs, powers, num=10 ** 14):
+def solve(powers, constant_args, maxes, num=10 ** 14):
     z = -1
+    fun_gen = search(num, powers, maxes, constant_args)
+    num_gen = nonzero_subtract(num)
     while z != 0:
-        num = nonzero_subtract(num)
-        z = search(num, funs, powers)
+        z = next(fun_gen)
+        num = next(num_gen)
+        fun_gen.send(num)
     return num
 
 
-# Parse each block of instructions into Python statements
-code = [
-    compile(
-        "; ".join(
-            re.sub(r"(?<![a-z])([a-z])(?![a-z])", r"state['\1']", extract_code(y))
-            for y in x
-        ),
-        "<string>",
-        "exec",
-    )
-    for x in processed
-]
-
-powers = [10 ** i for i in range(13, -1, -1)]
+"""Find the maximum possible z value in each block of the function in order to end with z = 0"""
 
 
-functions = list(map(constructor2, code))
+def get_max_zs(prev, argsets):
+    maxes = {}
+    n = len(argsets) - 1
+    for k, v in reversed(argsets.items()):
+        max_z = 0
+        # Have to reverse indices here, since this is working backwards of order blocks are actually run
+        for w in range(1, 10):
+            for z in range(prev, (prev * 10) + 10000):
+                new = block(w, z, **v)
+                max_z = z if new == prev else max_z
+        maxes[n - k] = prev = max_z
+    return maxes
 
-answer1 = solve(functions, powers=powers)
-# print(f"Answer 2 = {answer2}")
+
+def validate_zs(constants):
+    last = [set([0])]
+    for vals in reversed(constants.values()):
+        last.append(set())
+        for last_z in last[-2]:  # last filled set
+            for w in range(1, 10):
+                for x in range(2):
+                    # Solve for z
+                    z_min = (vals["c4"] * (last_z - x * (w + vals["c15"]))) / (
+                        25 * x + 1
+                    )
+                    if not (z_min % 2 == 0 and z_min > -1):
+                        continue
+                    z_max = z_min + vals["c4"]
+                    # Add any valid z-values not already present for this iteration
+                    last[-1].update(
+                        filter(
+                            lambda cand: int(((cand % 26 + vals["c5"])) != w) == x,
+                            range(int(z_min), int(z_max)),
+                        )
+                    )
+    return last
 
 
-# def constructor(code):
-# def inner(w, x, y, z):
-# exec(code)
-# return w, x, y, z
+# def find_highest(constants, zs)
+# num = 0
+# zs = reverse(zs)
+# for i, constants in enumerate(constants):
+# digits =
+
+
+zs = validate_zs(values)
+# funs = [wrap_block(constants) for constants in values]
+
+
+# maxes = get_max_zs(0, values)
+# powers = [10 ** i for i in range(13, -1, -1)]
+# answer1 = solve(powers, values, maxes)
+# print(f"Answer 1: {answer1}")
+
+
+# My approach when I worked backwards was to not be too clever. Try all w (1-9) and all values of x (0-1) and compute the range of possible inverses for that scenario. Then run the operation forwards for the candidates and throw away the bad ones. (edited)
+# [10:03 PM]
+# My first approach before improving to the above was to instead brute force. We have a set of target z to reach. Loop over all w (1-9) and a large range of z (e.g. 0-1000000) and run the operation to see if it hits our set of target z. All inputs that work make up our new set of target z.
 #
-# return inner
-# return inner
+# It's unsatisfyingly slow, but it works. (edited)
+# [10:10 PM]
+# For both approaches, when you have the ok z values for every step you can just greedily pick the largest/smallest digit that hits a z that is ok (edited)
+# January 25, 2022
+#
