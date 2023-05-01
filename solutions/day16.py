18eab2e19ee191df6fe424a05ba97fed0b58c7c0
ryan-heslin <rwheslin@gmail.com>
2023-05-01 10:58:32 -0400

    Clean up day 16

--- solutions/day16.py
+++ solutions/day16.py
@@ -1,19 +1,9 @@
+import operator
 from math import inf
 from math import prod
 
 with open("inputs/day16.txt") as f:
     raw_input = f.read().rstrip("\n")
-
-tests = [
-    "C200B40A82",
-    "04005AC33890",
-    "880086C3E88112",
-    "CE00C43D881120",
-    "D8005AC2A8F0",
-    "F600BC2D8F",
-    "9C005AC2F8F0",
-    "9C0141080250320F1802104A08",
-]
 
 
 def hex2bin(string):
@@ -90,6 +80,7 @@ def build_tree(digits, chars_left=inf, packets_left=inf):
         node.version = version
         node.type = type_id
         chars_left -= 6
+
         if type_id == 4:
             value, digits, consumed = parse_literal(digits)
             node.value = value
@@ -98,21 +89,19 @@ def build_tree(digits, chars_left=inf, packets_left=inf):
         else:
             length_id, digits = parse(digits, 1)
             chars_left -= 1
+            chars = 15 if length_id == 0 else 11
+            temp, digits = parse(digits, chars)
+            chars_left -= chars
+            prev = len(digits)
+
             if length_id == 0:
-                temp, digits = parse(digits, 15)
-                chars_left -= 15
-                prev = len(digits)
                 new, digits = build_tree(digits, chars_left=temp)
-                packets_left -= 1
-                chars_left -= prev - len(digits)
             else:
-                temp, digits = parse(digits, 11)
-                chars_left -= 11
-                prev = len(digits)
                 new, digits = build_tree(digits, packets_left=temp)
-                packets_left -= 1
-                chars_left -= prev - len(digits)
+            packets_left -= 1
+            chars_left -= prev - len(digits)
             node.add_child(new)
+
         out.append(node)
     return out, digits
 
@@ -132,16 +121,14 @@ def evaluate(node):
         return node.value
     assert node.value == 0
     for i, child in enumerate(node.children):
-        temp = evaluate(child)
-        node.children[i] = temp
+        node.children[i] = evaluate(child)
 
     # Binary or vector operation
-    if node.type in (5, 6, 7):
-        assert len(node.children) == 2
-        val = node.operators[node.type](*node.children)
-    else:
-        val = node.operators[node.type](node.children)
-    return val
+    arg = node.children
+    if node.type not in (5, 6, 7):
+        arg = [arg]
+        # assert len(node.children) == 2
+    return node.operators[node.type](*arg)
 
 
 digits = hex2bin(raw_input)
