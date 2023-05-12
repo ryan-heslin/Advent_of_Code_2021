0de65c56c386b6cd058c8e3245a24bc239923061
ryan-heslin <rwheslin@gmail.com>
2023-05-12 14:41:13 -0400

    Improve day 17

--- solutions/day17.py
+++ solutions/day17.py
@@ -1,59 +1,38 @@
 import re
+from math import copysign
+from math import inf
+
+
+def simulate(xvel, yvel):
+    x = y = 0
+    apogee = -inf
+    x_sign = -copysign(1, xvel) * bool(xvel)
+    hit = False
+
+    while y >= YMIN or yvel > 0:
+        x += xvel
+        y += yvel
+        apogee = max(y, apogee)
+        xvel += x_sign
+        x_sign *= bool(xvel)
+        yvel -= 1
+        if (not hit) and XMIN <= x <= XMAX and YMIN <= y <= YMAX:
+            hit = True
+
+    return apogee if hit else None
+
 
 with open("inputs/day17.txt") as f:
     raw_input = f.read()
 
-processed = [int(x) for x in re.findall(r"-?\d+", raw_input)]
-xmin, xmax, ymin, ymax = processed
+XMIN, XMAX, YMIN, YMAX = map(int, re.findall(r"-?\d+", raw_input))
+apogees = [simulate(x, y) for x in range(XMAX + 1) for y in range(YMIN - 1, 2000)]
+
+assert len(apogees)
+apogees = [x for x in apogees if x is not None]
+part1 = max(apogees)
+part2 = len(apogees)
 
 
-class Trajectory:
-    def __init__(self, x, y, xmin, xmax, ymin, ymax):
-        self.x = self.y = 0
-        self.apogee = -10000000
-        self.hit = False
-        self.xvel = x
-        self.yvel = y
-        self.x_sign = 1 if x < 0 else -1 if x > 0 else 0
-        self.xmin, self.xmax, self.ymin, self.ymax = xmin, xmax, ymin, ymax
-
-    # Simulate Trajectory; return True if target entered, false if beyond bounds
-    def step(self, mode):
-        while True:
-            self.x += self.xvel
-            self.y += self.yvel
-            self.apogee = max(self.y, self.apogee)
-            self.xvel += self.x_sign
-            self.x_sign = 0 if self.xvel == 0 else self.x_sign
-            self.yvel += -1
-            if (
-                self.x >= self.xmin
-                and self.x <= self.xmax
-                and self.y >= self.ymin
-                and self.y <= self.ymax
-            ):
-                self.hit = True
-            if self.y < self.ymin and self.yvel <= 0:
-                if mode == "part1":
-                    return -1000 if not self.hit else self.apogee
-                else:
-                    return self.hit
-
-    def __repr__(self):
-        return f"x: {self.x}, y: {self.y}"
-
-
-trajectories = [
-    Trajectory(x, y, xmin, xmax, ymin, ymax)
-    for x in range(xmax + 1)
-    for y in range(-2000, 2000)
-]
-
-apogees = [traj.step("part1") for traj in trajectories]
-
-answer1 = max(apogees)
-
-answer2 = sum([traj.step("part2") for traj in trajectories])
-
-print(f"Answer 1 = {answer1}")
-print(f"Answer 2 = {answer2}")
+print(f"Answer 1 = {part1}")
+print(f"Answer 2 = {part2}")
